2ea01f59ec9460fdae7ae472ecd73d21521b7131
changnam go <changnma.go@gmail.com>
2021-08-14 16:54:16 +0900

    datasourceproxy 추가, sql statement 를 상세히 남김

---
+++ src/main/java/com/honsoft/spring/config/AdditionalConfig.java
@@ -0,0 +1,11 @@
+package com.honsoft.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ComponentScan
+//@PropertySource()
+public class AdditionalConfig {
+	//@Bean
+	
+}
--- src/main/java/com/honsoft/spring/config/SpringJdbcConfig.java
+++ src/main/java/com/honsoft/spring/config/SpringJdbcConfig.java
@@ -1,5 +1,7 @@
 package com.honsoft.spring.config;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,46 +21,61 @@ import com.honsoft.spring.StudentDAO;
 import com.honsoft.spring.StudentDAOImpl;
 import com.zaxxer.hikari.HikariDataSource;
 
+import net.ttddyy.dsproxy.support.ProxyDataSource;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+
 @Configuration
 @ComponentScan("com.honsoft.spring")
 @PropertySource("classpath:/app.properties")
 public class SpringJdbcConfig {
 	@Autowired
 	Environment env;
-	
-    @Bean
-    public HikariDataSource mysqlDataSource() {
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/quickguide");
-        dataSource.setUsername("shoppingnt");
-        dataSource.setPassword("Shoppingnt2021!@");
-        return new HikariDataSource(dataSource);
-    }
-    
-    @Bean
-	public DataSourceInitializer dataSourceInitializer(@Qualifier("mysqlDataSource") DataSource datasource) {
+
+	@Bean
+	public HikariDataSource mysqlDataSource() {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/quickguide");
+		dataSource.setUsername("shoppingnt");
+		dataSource.setPassword("Shoppingnt2021!@");
+		return new HikariDataSource(dataSource);
+	}
+
+	@Bean
+	public DataSourceInitializer dataSourceInitializer(@Qualifier("mysqlProxyDataSource") DataSource datasource) {
 		ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
 		resourceDatabasePopulator.addScript(new ClassPathResource("mybatis/mysql/schema-mysql.sql"));
 		resourceDatabasePopulator.addScript(new ClassPathResource("mybatis/mysql/data-mysql.sql"));
-		//resourceDatabasePopulator.addScript(new ClassPathResource("ddl/common/user-data.sql"));
-		//resourceDatabasePopulator.addScript(new ClassPathResource("ddl/common/order-data.sql"));
+		// resourceDatabasePopulator.addScript(new
+		// ClassPathResource("ddl/common/user-data.sql"));
+		// resourceDatabasePopulator.addScript(new
+		// ClassPathResource("ddl/common/order-data.sql"));
 
 		DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
 		dataSourceInitializer.setDataSource(datasource);
 		dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
 		dataSourceInitializer.setEnabled(env.getProperty("mysql.datasource.initialize", Boolean.class, false));
-         
+
 		return dataSourceInitializer;
 	}
-    
-    
-    @Bean 
-    public StudentDAO studentDAOImpl() {
-    	StudentDAOImpl studentJDBCTemplate = new StudentDAOImpl();
-    	studentJDBCTemplate.setDataSource(mysqlDataSource());
-    	return studentJDBCTemplate;
-    }
-    
-    
+
+	@Bean
+	public StudentDAO studentDAOImpl() {
+		StudentDAOImpl studentJDBCTemplate = new StudentDAOImpl();
+		studentJDBCTemplate.setDataSource(mysqlProxyDataSource());
+		return studentJDBCTemplate;
+	}
+
+	@Bean
+	public ProxyDataSource mysqlProxyDataSource() {
+		ProxyDataSource dataSource = ProxyDataSourceBuilder.create(mysqlDataSource()) // pass original datasource
+				.logQueryBySlf4j() // logQueryBySlf4j(), logQueryByJUL(), logQueryToSysOut()
+				.countQuery() // enable query count metrics
+				.logSlowQueryBySlf4j(10, TimeUnit.MINUTES) // also by sl4j, jul, system out
+				.proxyResultSet() // enable proxying ResultSet
+				// .listener(myListener) // register my custom listener
+				.build();
+
+		return dataSource;
+	}
 }
